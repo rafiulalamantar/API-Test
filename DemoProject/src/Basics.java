dc0af9e1b09de8b83a8ea532046d7c9365e9ffbb
Rafiul Alam Antar <rafiulantar@gmail.com>
2021-10-30 01:15:14 +0600

    new  file added, conversion file to byte to string

--- DemoProject/src/Basics.java
+++ DemoProject/src/Basics.java
@@ -4,20 +4,24 @@ import io.restassured.path.json.JsonPath;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.testng.Assert;
 
 import file.payload;
 
 public class Basics {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		// validate addPlace API is working as expected
 		
 		RestAssured.baseURI="https://rahulshettyacademy.com";
 		
 		String response =given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
-			.body(payload.AddPlace())
+			.body(new String(Files.readAllBytes(Paths.get("G:\\Software SQA\\API Test\\DemoProject\\payload.json"))))
 		.when().post("maps/api/place/add/json")	
 		.then().assertThat().statusCode(200).body("scope",equalTo("APP"))
 		    .header("server","Apache/2.4.18 (Ubuntu)").extract().response().asString();
