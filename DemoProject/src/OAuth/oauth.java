2f6a59a77e6a040b8d4c0619291f2fd40ec954d3
Rafiul Alam Antar <rafiulantar@gmail.com>
2021-12-16 23:43:07 +0600

    updated class with main class and simple hit request perform

--- DemoProject/src/OAuth/oauth.java
+++ DemoProject/src/OAuth/oauth.java
@@ -1,7 +1,16 @@
 package OAuth;
 
+import static io.restassured.RestAssured.given;
+
 public class oauth {
-	
-	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String response=given().queryParam("access_token", "")
+		.when()
+		.get("https://rahulshettyacademy.com/getCourse.php").asString();
+		System.out.println(response);
+
+	}
 
 }
