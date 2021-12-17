d42dd2cce843f63f103e5100ab211f1e50c459de
Rafiul Alam Antar <rafiulantar@gmail.com>
2021-12-17 23:49:55 +0600

    using this function that not encode the url because rest assured not under stand sign

--- DemoProject/src/OAuth/oauth.java
+++ DemoProject/src/OAuth/oauth.java
@@ -29,7 +29,8 @@ public class oauth {
 		
 		
 		
-		String accessTokenResponse=given() .queryParams("code","")
+		String accessTokenResponse=given().urlEncodingEnabled(false)
+				.queryParams("code",code)
 				.queryParams("client_id","692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
 				.queryParams("client_secret","erZOWM9g3UtwNRj340YYaK_W")
 				.queryParams("redirect_uri","https://rahulshettyacademy.com/getCourse.php")
