6f32aa3035d635bc177b487040a020609ac8fe55
Rafiul Alam Antar <rafiulantar@gmail.com>
2021-12-17 21:34:15 +0600

    Selenium jar added in pom.xml

--- DemoProject/src/OAuth/oauth.java
+++ DemoProject/src/OAuth/oauth.java
@@ -1,6 +1,7 @@
 package OAuth;
 
 import static io.restassured.RestAssured.given;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -10,10 +11,10 @@ import io.restassured.path.json.JsonPath;
 
 public class oauth {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","G:\\Software SQA\\chromedriver_win32\\chromedriver.exe");
-		WebDriver driver=(WebDriver) new ChromeDriver();
+		WebDriver driver= new ChromeDriver();
 		driver.get("https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&auth_url=https://accounts.google.com/o/oauth2/v2/auth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https://rahulshettyacademy.com/getCourse.php");
 		driver.findElement(By.cssSelector("input['type=email']")).sendKeys("rafiulantar@gmail.com");
 		driver.findElement(By.cssSelector("input['type=email']")).sendKeys(Keys.ENTER);
